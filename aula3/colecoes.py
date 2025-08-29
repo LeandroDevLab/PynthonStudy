e9b6b44538ce7f52a338f8ca1a617932ff17c419
LeandroDevLab <prof.oliota@gmail.com>
2025-08-28 22:48:26 -0300

    Coleções e matrizes

---
+++ aula3/colecoes.py
@@ -0,0 +1,23 @@
+# coleções de dicionários CONJUNTOS
+conjunto = set([4,6,8,3,2,5,7,9])
+print(conjunto)
+
+# tupla
+tupla = (3,2,4,5,6)
+print(tupla)
+
+#
+pessoa = {
+  'nome': 'Leandro',
+  'telefone': '9987-5453',
+  'endereço': 'Rua são miguel'
+}
+
+print(pessoa['nome'])
+
+pessoas = [
+{'nome': 'Leandro','telefone': '9987-5453','endereço': 'Rua são miguel'},
+{'nome': 'Leandro','telefone': '9987-5453','endereço': 'Rua são miguel'}
+]
+
+print(pessoas[0]['telefone'])
